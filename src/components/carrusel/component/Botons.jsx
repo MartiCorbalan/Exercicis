2ca24465b0ce3f805c66eac8b670350b824b3de3
LAB1INF-05\Administrador <martilluc01@gmail.com>
2022-02-21 20:31:46 +0100

    Updates

--- src/components/carrusel/component/Botons.jsx
+++
@@ -1,7 +0,0 @@
-export default function Botons({ funcioClicar, value, value2 }) {
-  return (
-    <button className="botones" onClick={() => funcioClicar()} value2={value2}>
-      {value}
-    </button>
-  );
-}
--- src/components/graella/component/Imagen.jsx
+++
@@ -1,9 +0,0 @@
-export default function Imagen({ funcioClicar, src, clas, nimg }) {
-  return (
-    <img
-      className={"Avatar " + clas}
-      src={src}
-      onClick={() => funcioClicar(nimg)}
-    />
-  );
-}
