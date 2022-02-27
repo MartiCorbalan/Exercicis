658a62553318b4cb32e9127f1880643ce96bf338
MartiCorbalan <marti.corbalan@mataro.epiaedu.cat>
2022-02-27 21:14:24 +0100

    reducer

--- src/components/graella/codigo_correcto_graella.jsx
+++ src/components/graella/codigo_correcto_graella.jsx
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
-import { useReducer } from "react";
 
 import "./App.css";
 
-/* import Imagen from "./component/Imagen"; */
+import Imagen from "./component/Imagen";
 
 import { useState } from "react";
 
 function Graella() {
-
-    const [count, dispatch] = useReducer((input, posicio) => input == posicio, 0);
-
   let imatges = [
     "https://www.bbva.com/wp-content/uploads/2021/03/animales-peligro-extincion-sostenibilidad-bbva.jpg",
 
@@ -21,15 +17,16 @@ function Graella() {
     "https://www.fundacion-affinity.org/sites/default/files/dia-de-los-animales.jpg",
   ];
 
-  /* const [posicio, setPosicio] = useState(null);*/
-  /* const fullscreen = (input) => {
+  const [posicio, setPosicio] = useState(null);
+
+  const fullscreen = (input) => {
     if (input === posicio) {
       setPosicio(null);
     } else {
       console.log("click" + input);
       setPosicio(input);
     }
-  }; */
+  };
 
   return (
     <div className="App">
@@ -42,11 +39,11 @@ function Graella() {
               imatge,
               index //imatge es el valor i index la posicio del array
             ) => (
-              <img
+              <Imagen
                 src={imatge}
-                onClick={dispatch()}
+                funcioClicar={fullscreen}
                 clas={
-                  index === count
+                  index === posicio
                     ? "fullscreen" //si la posicion es la misma que la imagen, aplicamos la classe fullscreen
                     : ""
                 }
